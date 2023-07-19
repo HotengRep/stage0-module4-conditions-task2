307fa31e4e0cac15aa57af12f5d488ceab0b624e
Makar <hoteng789@gmail.com>
2023-07-19 12:30:28 +0400

    Changes in two classes CoinFlip.java GreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -3,5 +3,13 @@ package school.mjc.stage0.conditions.task2;
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
 
+        String result = "Tail";
+
+        if(from1UpTo1000 < 500)
+        {
+            result = "Eagle";
+        }
+
+        System.out.println(result);
     }
 }
